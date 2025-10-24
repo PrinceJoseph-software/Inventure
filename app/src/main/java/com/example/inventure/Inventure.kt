807ed835c3f79cbb2afd1df23e4101e49621e2ed
Soronnadi Nice <soronnadinice@gmail.com>
2025-10-24 23:09:56 +0100

    AddedParts

--- app/src/main/java/com/example/inventure/Inventure.kt
+++ app/src/main/java/com/example/inventure/Inventure.kt
@@ -1,2 +1,21 @@
 package com.example.inventure
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+
+@Entity(tableName = "inventures")
+data class Inventure(
+       @PrimaryKey(autoGenerate = true)
+        val id: Int = 0,
+        val name: String,
+        val description: String,
+        val quantity: Int = 0,
+        val price: Double = 0.0,
+        val category: String,
+        val supplier: String,
+        val dateAdded: Long = System.currentTimeMillis()
+    )
+
+
